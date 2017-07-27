1208c7a73753b52fc3159a566ab015d14bdf82e0
maurolepore <maurolepore@gmail.com>
2017-07-27 11:43:00 -0500

    Remove file to try

--- R/tab_ab12.R
+++
@@ -1,8 +0,0 @@
-# After saving the snippet (https://goo.gl/3VPZvc.) type tab and hit tab.
-# You should see something like this: https://goo.gl/xoACsn
-tab_ab12 <- "Descriptoin (one line)."
-
-tab_bci_data_year12_full_7 <- tibble::as_tibble(bci::bci12full7)
-
-fig_id <- "Descriptoin (one line)."
-
