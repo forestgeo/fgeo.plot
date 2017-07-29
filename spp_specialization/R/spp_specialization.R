f8734981c9b51d73d9950ef0a444eb7946b6caed
maurolepore <maurolepore@gmail.com>
2017-07-29 14:20:24 -0500

    Clean code

--- spp_specialization/R/spp_specialization.R
+++ spp_specialization/R/spp_specialization.R
@@ -8,22 +8,7 @@ library(ggplot2)
 
 
 
-
 sig_spp <- read_csv("./data/spp_specialization.csv")
-sig_spp <- select(sig_spp, 1, 2)
-sig_spp <- mutate(
-  sig_spp, 
-  spp_spec_4 = rep(c("LS", "P", "G", "F"), 5)
-)
 
-
-ggplot(
-  sig_spp, 
-  aes(x = sp, y = PN.sq.change.N1, fill = spp_spec_4)
-) +
+ggplot(sig_spp, aes(x = sp, y = PN.sq.change.N1, fill = spp_spec_4)) +
   geom_bar(stat = "identity")
-
-
-
-
-
