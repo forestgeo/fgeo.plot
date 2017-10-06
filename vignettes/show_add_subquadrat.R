e85ccf4f5c15558e82a89ef7626837b7fb5d76a2
maurolepore <maurolepore@gmail.com>
2017-10-06 17:43:06 -0400

    Dissable building vignettes via Build & Reload
    * Vignettes use older function add_subquadrat() that brakes build.
    
    * I plan to amend the vignettes once all previous code use the new version of add_subquadrat().

--- vignettes/show_add_subquadrat.R
+++ vignettes/show_add_subquadrat.R
@@ -1,19 +1,23 @@
-#' Add a quadrat variable to a dataframe based based on QX and QY coordinates.
-#'
-#' @param df A dataframe.
-#' @param dim_x,dim_y Quadrat dimension for the plot. For example, in Sinharaja
-#'   both are 20.
-#' @param divide_x,divide_y Total number of divisions of each quadrat side. For
-#'   example, For Sinharaja and most other plots, the value of these arguments
-#'   is 5, which results in a 4x4 grid of subquadrats within each quadrat.
-#' @return A dataframe with the additional variable `subquadrat`.
-#' @author Anudeep Singh.
-#' @export
-#'
-#' @examples
-#' df <- sinharaja::sinh_vftbl_selected
-#' with_subquadrat <- add_subquadrat(df, 20, 20, 5, 5)
-#' head(with_subquadrat)
+## ----setup, echo = FALSE, message=FALSE, warning=FALSE-------------------
+# hadley's settings
+set.seed(1014)
+options(digits = 3)
+
+knitr::opts_chunk$set(
+  echo = TRUE,  # {mine}
+  comment = "#>",
+  collapse = TRUE,
+  # cache = TRUE,
+  out.width = "70%",
+  fig.align = "center",
+  fig.width = 6,
+  fig.asp = 0.618,  # 1 / phi
+  fig.show = "hold"
+)
+
+options(dplyr.print_min = 6, dplyr.print_max = 6)
+
+## ------------------------------------------------------------------------
 add_subquadrat <- function(df, dim_x, dim_y, divide_x, divide_y) {
   # Simplify nested parentheses
   dim_x_mns.1 <- dim_x - 0.1
@@ -48,3 +52,23 @@ add_subquadrat <- function(df, dim_x, dim_y, divide_x, divide_y) {
   )
   with_subquadrat
 }
+
+## ------------------------------------------------------------------------
+library(tidyverse)
+
+df <- sinharaja::sinh_vftbl_selected
+with_subquadrat <- add_subquadrat(
+  df, dim_x = 20, dim_y = 20, divide_x = 5, divide_y = 5
+)
+
+q15 <- with_subquadrat %>%
+  filter(QuadratName == "0015")
+odds <- q15 %>%
+  filter(QX >= 20 | QY >= 20)
+
+ggplot(data = q15, aes(QX, QY)) +
+  geom_hline(yintercept = 20) +
+  geom_vline(xintercept = 20) +
+  geom_point(data = odds, colour = "red", size = 8) +
+  geom_text(data = q15, aes(label = subquadrat))
+
