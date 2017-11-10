0b8de4652fd24c4745f9af40ffd75a65b83e3b4a
maurolepore <maurolepore@gmail.com>
2017-11-10 12:29:31 -0500

    Declare global variable .data from rlang

---
+++ R/utils-import_and_globalVariables.R
@@ -0,0 +1,6 @@
+
+# rlang -------------------------------------------------------------------
+
+# Flag inline helpers as global variables so R CMD check doesn't warn
+utils::globalVariables(c(".data"))
+
