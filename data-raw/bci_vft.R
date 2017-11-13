39b78f0b9b61c802027cdcadf3f400bfd5c7a85b
maurolepore <maurolepore@gmail.com>
2017-11-13 14:44:55 -0500

    Amend example viewfulltable

--- data-raw/bci_vft.R
+++ data-raw/bci_vft.R
@@ -1,23 +1,25 @@
-# Data of a few quadrats of the census 7, in the ViewFullTable from Barro
+# ViewFullTable with data of a few quadrats of the 50 hectare plot from Barro
 # Colorado Island downloaded from: https://doi.org/10.5479/data.bci.20130603.
-# This data is for examples.
+# Explanation by Suzanne Lao https://goo.gl/fg2nqh.
+
+# 1. Download (from https://doi.org/10.5479/data.bci.20130603) and store locally
 
 library(tidyverse)
 
-# After downloading the data from the link above, here I load all the data from
-# a local repository
-
-
+# Load from local
 bci_vft <- tibble::as.tibble(
   readr::read_tsv("../bci/data-raw/ViewFullTable/ViewFullTable.txt")
 )
 
-# Filter only the last census to save space
-bci12vft7 <- bci_vft %>%
-  filter(bci_vft$PlotCensusNumber == 7)
+# Censuses 6 and 7 of the 50 hectare plot
+bci12vft_mini <- bci_vft %>%
+  filter(
+    PlotID == 1,
+    PlotCensusNumber %in% c(6, 7),
+    !is.na(QuadratName),
+  ) %>%
+  filter(QuadratName %in% unique(.$QuadratName)[1:10])
 
-quads <- unique(bci12vft7$QuadratName)[1:10]
-bci12vft7mini <- bci12vft7 %>% filter(QuadratName %in% quads)
+use_data(bci12vft_mini)
 
-use_data(bci12vft7mini)
 
