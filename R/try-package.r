14810cd9b60b083be1290473a9ed8e265588c05a
maurolepore <maurolepore@gmail.com>
2017-11-28 13:59:39 -0500

    Use simplest package doc, and let roxygen copy info from DESCRIPTION.

--- R/try-package.r
+++ R/try-package.r
@@ -1,10 +1,5 @@
-#' Try new things.
-
-#' Try makes if  easy to share code to try new things. It aims to draft a
-#' solution to address a very specific issue.
-#'
-#' @name try
-#' @docType package
-#' @name forestr
-#' @docType package
+#' @keywords internal
+#' @import ggplot2
+#' @importFrom rlang .data
+#' @importFrom ggrepel geom_text_repel
 "_PACKAGE"
