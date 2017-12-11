d021f7c955ade9715c16ca1c202a4fb3ac86e7ac
maurolepore <maurolepore@gmail.com>
2017-12-11 14:50:27 -0500

    Add bciex datasets to data/

---
+++ data-raw/bciex.R
@@ -0,0 +1,12 @@
+# source: https://github.com/forestgeo/bciex
+
+library(bciex)
+
+usethis::use_data(bci12s6mini)
+usethis::use_data(bci12s7mini)
+
+usethis::use_data(bci12t6mini)
+usethis::use_data(bci12t7mini)
+
+usethis::use_data(bci_elevation)
+
