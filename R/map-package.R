a8a21300488d3f7e88daec0021c395aa886a8174
maurolepore <maurolepore@gmail.com>
2017-12-13 14:41:02 -0500

    Add global variables to silent CMD check notes

--- R/map-package.R
+++ R/map-package.R
@@ -4,3 +4,8 @@
 #' @importFrom ggrepel geom_text_repel
 "_PACKAGE"
 
+
+
+# Flag inline helpers as global variables so R CMD check doesn't warn
+utils::globalVariables(c(".data", "gx", "gy", "qx", "qy", "status_tree",
+  "subquadrat", "elev", "..level.."))
