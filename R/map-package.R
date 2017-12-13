c8c54a990a26c339550a305438e80d50de329d36
maurolepore <maurolepore@gmail.com>
2017-12-13 14:26:09 -0500

    Read first definition of global variables.

--- R/map-package.R
+++ R/map-package.R
@@ -4,11 +4,3 @@
 #' @importFrom ggrepel geom_text_repel
 "_PACKAGE"
 
-
-
-# Flag inline helpers as global variables so R CMD check doesn't warn
-utils::globalVariables(
-  c(".data", "qx", "qy", "status_tree", "subquadrat", "elev", "..level..")
-)
-
-
