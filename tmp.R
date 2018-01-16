bbe3cb7f3e0333132ab752bd4f4a5cc58f6bf30d
maurolepore <maurolepore@gmail.com>
2018-01-16 12:05:07 -0500

    Fixes #25. Now map_tag() plots the right symbol if data has single status.

--- tmp.R
+++ tmp.R
@@ -1,27 +1,15 @@
 library(tidyverse)
 library(fgeo)
 
-vft <- sinharaja::sinh_vft %>% 
-  as_tibble() %>% 
-  top(PlotID) %>% 
-  top(CensusID, -2) %>% 
-  filter(Status == "dead") %>% 
-  filter(QX > 20 | QY > 20) %>% 
-  arrange(QuadratID, QX, QY) %>% 
-  select(QuadratID, QX, QY, Tag, Status, everything())
-vft
+vft <- sinharaja::sinh_vft %>%
+  as_tibble() %>%
+  top(PlotID) %>%
+  top(CensusID, -1) %>%
+  select(Tag, Status, everything())
 
-quad_spillover <- vft %>% 
-  count(QuadratID) %>%
-  arrange(desc(n)) %>% 
-  .[1, ] %>% 
-  pull(QuadratID)
-  pull(QuadratID)[[1]]
-  filter(QuadratID == 489) 
-
-demo <- vft %>% dplyr::filter(QuadratID == quad_spillover)
-p <- map_tag(demo)
-
-pdf("map.pdf", paper = "letter", height = 10.5, width = 8)
-p[2]
-dev.off()
+vft %>%
+  # filter(Status == "alive") %>%
+  # filter(Status == "dead") %>%
+  top(QuadratID) %>%
+  map_tag() %>%
+  first()
