b6c4aa45cfe3ebffdb4db86b4ad018c3ada916f2
maurolepore <maurolepore@gmail.com>
2018-01-17 15:31:39 -0500

    Update test map_plot()

--- tests/testthat/test-map_plot.R
+++ tests/testthat/test-map_plot.R
@@ -2,16 +2,15 @@ library(ggplot2)
 
 bad_nms_elev <- bciex::bci_elevation
 bci_elev <- dplyr::rename(bad_nms_elev, gx = x, gy = y)
-context("map_plot")
 
-# Tests are in map_base
-
-context("map_base.R")
+context("map_plot.R")
 
 test_that("outputs as a ggplot", {
   # Just in case, calling map_plot()
-  expect_silent(p <- map_plot(bci_elev))
-  
+  expect_silent(
+    p <- map_plot(bci_elev)
+  )
+
   expect_true(
     any(grepl("ggplot", class(p)))
   )
@@ -19,15 +18,15 @@ test_that("outputs as a ggplot", {
 
 test_that("errs with wrong input", {
   expect_error(
-    map_base(1)
+    map_plot(1)
   )
   expect_error(
-    map_base(bad_nms_elev)
+    map_plot(bad_nms_elev)
   )
   expect_error(
-    map_base(bci_elev, xlim = -1)
+    map_plot(bci_elev, xlim = -1)
   )
   expect_error(
-    map_base(bci_elev, theme = "wrong")
+    map_plot(bci_elev, theme = "wrong")
   )
 })
