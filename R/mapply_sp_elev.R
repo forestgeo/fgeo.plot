e83907b40cd3a288f7e689f04ada5e643447af81
maurolepore <maurolepore@gmail.com>
2018-01-24 20:05:27 -0500

    Write examples.

--- R/mapply_sp_elev.R
+++ R/mapply_sp_elev.R
@@ -47,6 +47,50 @@
 #' @export 
 #'
 #' @examples
+#' census <- fgeo.tool::top(bciex::bci12s7mini, sp, 2)
+#' 
+#' elevation <- bciex::bci_elevation
+#' head(elevation)
+#' elevation <- fgeo.tool::clean_structure_elev(bciex::bci_elevation)
+#' head(elevation)
+#' 
+#' # Showing first plot only. 
+#' p <- mapply_sp_elev(census)[[1]]
+#' p[[1]]
+#' 
+#' 
+#' p <- mapply_sp_elev(
+#'   census,
+#'   elevation,
+#'   species = spp,
+#'   fill = "white",
+#'   shape = 21,
+#'   point_size = 5,
+#'   contour_size = 1,
+#'   low = "grey",
+#'   high = "black",
+#'   hide_legend_elev = TRUE,
+#'   bins = 7,
+#'   label_elev = FALSE
+#' )
+#' p[[1]]
+#' 
+#' # Same but outputs a plot, not a list of plots
+#' map_sp_elev(census, elevation)
+#' 
+#' # Similar but maps elevation exclusively
+#' map_elev(elevation)
+#' 
+#' # For maximum control, you can compose maps as you like
+#' map_gx_gy_elev(elevation) %>% 
+#'   limit_gx_gy(xlim = c(0, 1200)) %>% 
+#'   contour_elev(contour_size = 0.5) %>% 
+#'   label_elev(label_color = "red") %>% 
+#'   hide_axis_labels() %>% 
+#'   hide_legend_elev() %>% 
+#'   add_sp(census, point_size = 5) %>% 
+#'   facet_h_sp() %>% 
+#'   theme_default(legend.position = "top")
 mapply_sp_elev <- function(census,
                            elevation = NULL,
                            species = "all",
@@ -292,7 +336,7 @@ contour_elev <- function(p,
                          contour_size = 1, 
                          low = "blue", 
                          high = "red", 
-                         bins = bins) {
+                         bins = NULL) {
    p +
     stat_contour(
       aes(x = gx, y = gy, z = elev, colour = ..level..), 
