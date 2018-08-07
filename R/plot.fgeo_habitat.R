1bc986efa322f3068ed4b5447a6108a77b86ac93
maurolepore <maurolepore@gmail.com>
2018-08-07 14:23:59 -0400

    New plot.fgeo_habitat().

---
+++ R/plot.fgeo_habitat.R
@@ -0,0 +1,20 @@
+#' Plot objects of class "fgeo_habitat".
+#'
+#' @param x An S3 object of class "fgeo_habitat".
+#' @param ... Other arguments passed to methods.
+#'
+#' @return An object of class "ggplot".
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' library(fgeo.tool)
+#' elev_list <- fgeo.data::luquillo_elevation
+#' habitats <- fgeo_habitat(elev_list, gridsize = 20, n = 4)
+#' plot(habitats)
+#' }
+plot.fgeo_habitat <- function(x, ...) {
+  ggplot(x, aes(x = gx, y = gy)) +
+    geom_raster(aes(fill = habitats)) + 
+    coord_fixed()
+}
---
+++ tests/testthat/test-plot-fgeo_habitat.R
@@ -0,0 +1,14 @@
+context("test-plot-fgeo_habitat")
+
+describe("outputs an object of class ggplot", {
+  skip_if_not_installed("fgeo.tool")
+  library(fgeo.tool)
+
+  elev_list <- fgeo.data::luquillo_elevation
+  habitats <- fgeo_habitat(elev_list, gridsize = 20, n = 4)
+  p <- plot(habitats)
+  
+  it("does something I want it to do", {
+    expect_is(p, "ggplot")
+  })
+})
