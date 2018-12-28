5eed6855d341f3022046d22ac4a87a1cf7684aa4
maurolepore <maurolepore@gmail.com>
2018-12-28 18:15:51 -0500

    Improve examples and docs of pick_vft().

--- R/pick_vft.R
+++ R/pick_vft.R
@@ -1,29 +1,33 @@
-#' Pick ViewFullTables to create small example datasets.
+#' Pick some censuses, quadrats and rows from a ViewFullTable.
 #' 
-#' This function helps you to create small datasets for examples. It defaults to
-#' returning the input ViewFullTable "as is", but provides arguments to pick a
-#' specific number of censuses, quadrats and rows.
+#' This function helps you to pick some rows of a ForestGEO ViewFullTable. It is
+#' useful to create small datasets for examples. It defaults to returning the
+#' input ViewFullTable "as is", but provides arguments to pick a specific number
+#' of censuses, quadrats and rows. 
 #' 
 #' @param .data A ViewFullTable
-#' @param n_censuses,n_quadrats Number of censuses and quadrats to pick.
-#' @param n_rows Number of rows to sample after picking `n_censuses` and
-#'   `n_quadrats`.
+#' @param n_censuses,n_quadrats Number of censuses and quadrats to pick from the
+#'   front (if positive) or back (if negative) of the unique alpha-sorted values
+#'   of `CensusID` and `QuadratID`, respectively (see examples below).
+#' @param n_rows Number of rows to sample at random -- after picking
+#'   `n_censuses` and `n_quadrats`.
 #'
 #' @return A tibble (dataframe).
 #' 
 #' @examples
-#' # Same
 #' dim(fgeo.x::vft_4quad)
+#' # Same
 #' dim(pick_vft(fgeo.x::vft_4quad))
 #' 
-#' dim(
-#'   pick_vft(
-#'     fgeo.x::vft_4quad, 
-#'     n_censuses = 2, 
-#'     n_quadrats = 2, 
-#'     n_rows = 50
-#'  )
-#' )
+#' dim(pick_vft(fgeo.x::vft_4quad, n_rows = 50))
+#' 
+#' sort(unique(fgeo.x::vft_4quad$QuadratID))
+#' 
+#' # Pick from the front
+#' unique(pick_vft(fgeo.x::vft_4quad, n_quadrats = 1)$QuadratID)
+#' 
+#' # Pick from the back
+#' unique(pick_vft(fgeo.x::vft_4quad, n_quadrats = -2)$QuadratID)
 #' 
 #' @family functions to pick or drop rows of a ForestGEO dataframe
 #' @family functions for fgeo vft
