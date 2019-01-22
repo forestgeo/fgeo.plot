0d07e3ef317764cbbbbb6a38828ddfc54baf19af
Mauro Lepore <maurolepore@gmail.com>
2019-01-22 14:06:05 -0500

    Update docs

--- man-roxygen/compare_ggplot2.R
+++ man-roxygen/compare_ggplot2.R
@@ -1,3 +1,3 @@
 #' @details
-#' \strong{fgeo.plot} wraps the \strong{ggplot2} package. For more control you
-#' can use \strong{ggplot2} directly.
+#' \strong{fgeo.plot} wraps some functions from the \strong{ggplot2} package.
+#' For more control you can use \strong{ggplot2} directly.
