460a3fc2f23068db60246495431da52c876142f2
Mauro Lepore <maurolepore@gmail.com>
2019-03-01 10:26:42 -0500

    In `sp_elev()`, clarify that `sp` and `elev` should come from the same forest plot (Closes #49; thanks @mcgregorian1 and @alyssaterrell).
    * Improve docs.
    * Warn if `sp` and `elev` have different plotdim.

--- R/sp_elev.R
+++ R/sp_elev.R
@@ -48,7 +48,9 @@ elev <- function(elev) {
 #'
 #' @param sp A ForestGEO-like dataframe with the column `sp`.
 #' @param elev A ForestGEO-like elevation list or its `col` dataframe -- with
-#'   the column `elev`.
+#'   the column `elev`. The datasets you pass to `sp` and `elev` should come
+#'   from the same forest plot. This is not compulsory but not doing so is most
+#'   likely a mistake.
 #'
 #' @seealso [autoplot.sp_elev()].
 #'
@@ -57,7 +59,10 @@ elev <- function(elev) {
 #' @examples
 #' assert_is_installed("fgeo.x")
 #' 
-#' species_elevation <- sp_elev(fgeo.x::stem5, fgeo.x::elevation)
+#' species_from_luquillo <- fgeo.x::stem5
+#' elevation_from_luquillo <- fgeo.x::elevation
+#' 
+#' species_elevation <- sp_elev(species_from_luquillo, elevation_from_luquillo)
 #' inherits(species_elevation, "sp_elev")
 #' @family plot functions
 #' @family autoplots
@@ -72,12 +77,37 @@ sp_elev <- function(sp, elev = NULL) {
 }
 
 validate_sp_elev <- function(sp, elev) {
-  if (!is.null(elev)) {
-    stopifnot(is.data.frame(fgeo.tool::fgeo_elevation(elev)))
-  }
-
   stopifnot(is.data.frame(sp))
   check_crucial_names(sp, c("gx", "gy", "sp"))
+  
+  if (!is.null(elev)) {
+    elev <- fgeo.tool::fgeo_elevation(elev)
+    stopifnot(is.data.frame(elev))
+    warn_if_plotdim_are_different(sp, elev)
+  }
+  
+  invisible(list(sp = sp, elev = elev))
+}
+
+warn_if_plotdim_are_different <- function(sp, elev) {
+  plotdim_elev <- purrr::quietly(fgeo.tool::guess_plotdim)(elev)
+  plotdim_sp <- purrr::quietly(fgeo.tool::guess_plotdim)(sp)
+  
+  identical_plotdim <- !identical(plotdim_sp$result, plotdim_elev$result)
+  if (identical_plotdim) {
+    warn(glue("
+      The datasets passed to `sp` and `elev` seem to have different dimensions.
+      Are you sure they come from the same forest plot?
+      * `sp` {rm_brake(plotdim_sp$messages)}
+      * `elev` {rm_brake(plotdim_elev$messages)}
+    "))
+  }
+  
+  invisible(list(sp = sp, elev = elev))
+}
+
+rm_brake <- function(x) {
+  sub("\n", "", x)
 }
 
 #' @export
