ae1ac35aa0755f705bb8f9ffd58f0f384b9854df
Mauro Lepore <maurolepore@gmail.com>
2019-05-25 06:45:47 -0400

    Adress CRAN request:
    Fix warning 'library' or 'require' call not declared from: 'fgeo'

--- tests/testthat/test-plot-fgeo_habitat.R
+++ tests/testthat/test-plot-fgeo_habitat.R
@@ -1,11 +1,11 @@
 context("autoplot-fgeo_habitat")
 
 describe("outputs an object of class ggplot", {
-  skip_if_not_installed("fgeo")
-  library(fgeo)
+  skip_if_not_installed("fgeo.x")
+  skip_if_not_installed("fgeo.analyze")
 
   elev_list <- fgeo.x::elevation
-  habitats <- fgeo_habitat(elev_list, gridsize = 20, n = 4)
+  habitats <- fgeo.analyze::fgeo_habitat(elev_list, gridsize = 20, n = 4)
   p <- autoplot(habitats)
 
   it("does something I want it to do", {
